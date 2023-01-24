3a40ff0f629fa05d6102a3f1eba6898dc853147e
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-24 17:15:08 +0200

    Add `Postgres` specific enums `OrderDirection` and `StationListOrder`

--- backend/src/database/postgres_view.rs
+++ backend/src/database/postgres_view.rs
@@ -1,3 +1,37 @@
 mod station;
 
 pub use station::*;
+pub use station_list::*;
+
+#[derive(Debug)]
+pub enum OrderDirection {
+    Ascending,
+    Descending,
+}
+
+impl ToString for OrderDirection {
+    fn to_string(&self) -> String {
+        let direction = match self {
+            Self::Ascending => "ASC",
+            Self::Descending => "DESC",
+        };
+
+        direction.to_string()
+    }
+}
+
+#[derive(Debug)]
+pub enum StationListOrder {
+    Id,
+}
+
+impl ToString for StationListOrder {
+    fn to_string(&self) -> String {
+        let column = match self {
+            Self::Id => "station_id"
+        };
+
+        column.to_string()
+    }
+}
+
