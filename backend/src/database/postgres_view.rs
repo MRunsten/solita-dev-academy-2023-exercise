5d73df954d303b351fc311f2c6873e8897f33539
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-24 18:23:41 +0200

    Add `JourneyListOrder` view parameter support for `Postgres`

--- backend/src/database/postgres_view.rs
+++ backend/src/database/postgres_view.rs
@@ -1,8 +1,10 @@
 mod station;
 mod station_list;
+mod journey_list;
 
 pub use station::*;
 pub use station_list::*;
+pub use journey_list::*;
 
 #[derive(Debug)]
 pub enum OrderDirection {
@@ -30,6 +32,21 @@ impl ToString for StationListOrder {
     fn to_string(&self) -> String {
         let column = match self {
             Self::Id => "station_id"
+        };
+
+        column.to_string()
+    }
+}
+
+#[derive(Debug)]
+pub enum JourneyListOrder {
+    DepartureDate,
+}
+
+impl ToString for JourneyListOrder {
+    fn to_string(&self) -> String {
+        let column = match self {
+            Self::DepartureDate => "departure_date"
         };
 
         column.to_string()
