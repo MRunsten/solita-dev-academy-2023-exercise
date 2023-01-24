cce4dca2c89c528a17954382201cf6590cbd1bf0
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-24 18:21:16 +0200

    Add time related fields to `view::JourneyListItem`
    Additionally unused import was removed.

--- backend/src/database/view.rs
+++ backend/src/database/view.rs
@@ -1,11 +1,14 @@
 use crate::model::{city, station, station_operator};
-use crate::unit::{Coordinate, Kilometers, Minutes};
+use crate::unit::{Kilometers, Minutes};
 
 #[cfg(feature = "postgres")]
 pub use crate::database::postgres_view::*;
 
 #[derive(Debug)]
 pub struct JourneyListItem {
+    pub departure_date: chrono::NaiveDateTime,
+    pub return_date: chrono::NaiveDateTime,
+
     pub departure_station: JourneyListItemStation,
     pub return_station: JourneyListItemStation,
 
