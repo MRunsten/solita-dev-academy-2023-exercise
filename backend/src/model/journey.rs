033c61d780cb6bc1e1be65e5b3738e9df156ed74
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-24 13:16:25 +0200

    Add extra struct `JourneyInsert` to use with database insertion

--- backend/src/model/journey.rs
+++ backend/src/model/journey.rs
@@ -17,22 +17,34 @@ pub struct Journey {
     pub duration: Seconds,
 }
 
-impl Journey {
+#[derive(Debug)]
+pub struct JourneyInsert {
+    pub departure_date: chrono::NaiveDateTime,
+    pub departure_station_id: station::Id,
+
+    pub return_date: chrono::NaiveDateTime,
+    pub return_station_id: station::Id,
+
+    pub distance: Meters,
+    pub duration: Seconds,
+}
+
+impl JourneyInsert {
     pub fn departure_before_return(&self) -> bool {
         self.departure_date < self.return_date
     }
 
-    pub fn duration_over(&self, limit: chrono::Duration) -> bool {
+    pub fn duration_over_or_exactly(&self, limit: Seconds) -> bool {
         self.duration >= limit
     }
 
-    pub fn distance_over(&self, limit: Meters) -> bool {
+    pub fn distance_over_or_exactly(&self, limit: Meters) -> bool {
         self.distance >= limit
     }
 
     pub fn is_valid(&self) -> bool {
         self.departure_before_return()
-            && self.duration_over_or_exactly(chrono::Duration::seconds(10))
-            && self.distance_over_or_exactly(Meters(10))
+            && self.duration_over_or_exactly(10.0 as Seconds)
+            && self.distance_over_or_exactly(10.0 as Meters)
     }
 }
