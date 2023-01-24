ebac10c5b3f9edb9de8bfc3ebb6c2dc635ea624a
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-24 15:46:58 +0200

    Add `postgres` database support for `view::Station`

---
+++ backend/queries/postgres/create_view_station.sql
@@ -0,0 +1,29 @@
+CREATE VIEW station_view AS
+    SELECT
+        stations.station_id,
+
+        stations.name_finnish,
+        stations.name_swedish,
+        stations.name_english,
+
+        stations.address_finnish,
+        stations.address_swedish,
+
+        cities.name_finnish as city_name_finnish,
+        cities.name_swedish as city_name_swedish,
+
+        stations.capacity,
+
+        departing_journeys.amount as journeys_departing_amount,
+        returning_journeys.amount as journeys_returning_amount
+    FROM
+        stations,
+        cities,
+        (SELECT departure_station_id, COUNT(*) as amount FROM journeys GROUP BY departure_station_id) as departing_journeys,
+        (SELECT return_station_id, COUNT(*) as amount FROM journeys GROUP BY return_station_id) as returning_journeys
+    WHERE
+        stations.station_id = departing_journeys.departure_station_id
+    AND
+        stations.station_id = returning_journeys.return_station_id
+    AND
+        stations.city_id = cities.city_id
--- backend/src/database/postgres_view/station.rs
+++ backend/src/database/postgres_view/station.rs
@@ -1,5 +1,40 @@
-use crate::database::{Database, view};
+use crate::database::{view, Database, DatabaseResult};
+use crate::model::{city, station};
 
-pub fn station(db: &Database) -> view::Station {
-    todo!()
+pub async fn station(db: &Database, station_id: station::Id) -> DatabaseResult<view::Station> {
+    let record = sqlx::query!(
+        "SELECT * FROM station_view WHERE station_id = $1",
+        i32::from(&station_id),
+    )
+    .fetch_one(db)
+    .await?;
+
+    // Note: The existence on the following columns is checked by sqlx during compile time,
+    // but the record type returns Option<T> values. This is because views in Postgres technically
+    // can contain NULL values and it requires quite a bit of trickery in order to never return
+    // NULL values from a view.
+    //
+    // This view should never return NULL (None) values.
+    let station_view = view::Station {
+        station_id,
+        name: station::Name {
+            finnish: record.name_finnish.unwrap_or("error".to_string()),
+            swedish: record.name_swedish.unwrap_or("error".to_string()),
+            english: record.name_english.unwrap_or("error".to_string()),
+        },
+
+        address: station::Address {
+            finnish: record.address_finnish.unwrap_or("error".to_string()),
+            swedish: record.address_swedish.unwrap_or("error".to_string()),
+        },
+
+        city: city::Name {
+            finnish: record.city_name_finnish.unwrap_or("error".to_string()),
+            swedish: record.city_name_swedish.unwrap_or("error".to_string())
+        },
+        total_starting_journeys: record.journeys_departing_amount.unwrap_or(0) as u64,
+        total_ending_journeys: record.journeys_returning_amount.unwrap_or(0) as u64
+    };
+
+    Ok(station_view)
 }
