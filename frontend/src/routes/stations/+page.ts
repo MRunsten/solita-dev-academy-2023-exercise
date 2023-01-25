23f71cfbc174ffee60dcee48855eb4c2f7329234
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-25 17:21:47 +0200

    Add display component for list of `Station`s with support for data fetching

--- frontend/src/routes/stations/+page.ts
+++ frontend/src/routes/stations/+page.ts
@@ -1,10 +1,47 @@
+import { z } from "zod";
+
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (({ params }) => {
-    return {
+import { PUBLIC_API_ADDRESS } from '$env/static/public'
 
-    };
+const StationListView = z.array(z.object({
+    station_id: z.number().transform(v => v.toString().padStart(3, '0')),
 
-    throw error(404, 'Not found');
+    name: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+        english: z.string(),
+    }),
+
+    city_name: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+    }),
+
+    operator_name: z.string(),
+
+    capacity: z.number(),
+}));
+
+export const load = (async ({ fetch }) => {
+    
+    const res = await fetch(`${PUBLIC_API_ADDRESS}/station/list`);
+    const station_json = await res.json();
+
+    if(!res.ok) {
+        throw error(res.status, {
+            message: station_json,
+        });
+    }
+
+    let parsed_station_list_view = StationListView.safeParse(station_json);
+
+    if (!parsed_station_list_view.success) {
+        throw error(500, {
+            message: parsed_station_list_view.error.message,
+        });
+    }
+
+    return {stations: parsed_station_list_view.data};
 }) satisfies PageLoad;
