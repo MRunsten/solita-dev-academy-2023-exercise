359e9effe80670dc8caf585e62d3ab21f2a2c64b
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-25 15:01:21 +0200

    Add CORS middleware to allow `/api/*` access from any address.
    This is currently always enabled. It is also possible to only
    allow access from anywhere during development and use a reverse
    proxy to enable access to different servers (backend, frontend)
    during production.

--- backend/src/api.rs
+++ backend/src/api.rs
@@ -1,20 +1,22 @@
-use std::env;
-use axum::Router;
-use axum::routing::get;
-use serde::Deserialize;
 use crate::BoxedError;
+use axum::routing::get;
+use axum::Router;
+use serde::Deserialize;
+use std::env;
+use axum::http::Method;
+use tower::ServiceBuilder;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::database::Database;
 
-pub mod station;
 pub mod journey;
+pub mod station;
 
 pub async fn run(db: Database) -> Result<(), BoxedError> {
-
     let app = define_routes(db);
 
-    let api_address = env::var("API_ADDRESS")
-        .expect("Environment variable API_ADDRESS was undefined");
+    let api_address =
+        env::var("API_ADDRESS").expect("Environment variable API_ADDRESS was undefined");
 
     let addr = api_address.parse()?;
     tracing::info!("Bicycle application backend listening on {}", addr);
@@ -28,21 +30,22 @@ pub async fn run(db: Database) -> Result<(), BoxedError> {
 }
 
 fn define_routes(db: Database) -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET])
+        .allow_origin(Any);
 
     let station_api = Router::new()
         .route("/:id", get(station::single))
         .route("/list", get(station::list));
 
-    let journey_api = Router::new()
-        .route("/list", get(journey::list));
+    let journey_api = Router::new().route("/list", get(journey::list));
 
     let api = Router::new()
         .nest("/journey", journey_api)
-        .nest("/station", station_api);
+        .nest("/station", station_api)
+        .layer(ServiceBuilder::new().layer(cors));
 
-    let app = Router::new()
-        .nest("/api", api)
-        .with_state(db);
+    let app = Router::new().nest("/api", api).with_state(db);
 
     app
 }
