68289d18aa8a785d1ac6736a254b081304872253
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-25 17:03:06 +0200

    Add `.env` variable support for frontend
    Note that only PUBLIC_* prefixed environment variables are passed
    into UI.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,6 +9,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		env: {
+			dir: "../",
+		},
 		adapter: adapter()
 	}
 };
