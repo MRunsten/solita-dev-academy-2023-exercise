168f6ee996e35de3c1c68c1a99363e5cad62219a
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-25 17:05:29 +0200

    Add display component for `Station` with support for data fetching

--- frontend/src/routes/station/[id]/+page.svelte
+++ frontend/src/routes/station/[id]/+page.svelte
@@ -4,4 +4,35 @@
     export let data: PageData;
 </script>
 
-<h1>Single station: {data.station_id}</h1>
+<h1>Station ({data.station_id})</h1>
+
+<table>
+    <tr>
+        <td>Name</td>
+        <td>{data.name.finnish} | {data.name.swedish} | {data.name.english}</td> 
+    </tr>
+    
+    <tr><!-- Todo add check for city name with a single whitespace " " -->
+        <td>City</td>
+        <td>{data.city.finnish} | {data.city.swedish}</td>
+    </tr>
+   
+    <tr>
+        <td>Address</td>
+        <td>{data.address.finnish} | {data.address.swedish}</td>
+    </tr>
+</table>
+
+<br>
+
+<table>
+    <tr>
+        <td>Total ending journeys to this station</td>
+        <td>{data.total_ending_journeys}</td>
+    </tr>
+
+    <tr>
+        <td>Total starting journeys from this station</td>
+        <td>{data.total_starting_journeys}</td>
+    </tr>
+</table>
--- frontend/src/routes/station/[id]/+page.ts
+++ frontend/src/routes/station/[id]/+page.ts
@@ -1,10 +1,51 @@
+import { z } from "zod";
+
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (({ params }) => {
-    return {
-        station_id: params.id,
-    };
+import { PUBLIC_API_ADDRESS } from '$env/static/public'
 
-    throw error(404, 'Not found');
+const StationView = z.object({
+    station_id: z.number().transform(v => v.toString().padStart(3, '0')),
+
+    name: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+        english: z.string(),
+    }),
+
+    city: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+    }),
+
+    address: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+    }),
+
+    total_ending_journeys: z.number(),
+    total_starting_journeys: z.number(),
+})
+
+export const load = (async ({ fetch, params }) => {
+    
+    const res = await fetch(`${PUBLIC_API_ADDRESS}/station/${params.id}`);
+    const station_json = await res.json();
+
+    if(!res.ok) {
+        throw error(res.status, {
+            message: station_json,
+        });
+    }
+
+    let parsed_station_view = StationView.safeParse(station_json);
+
+    if (!parsed_station_view.success) {
+        throw error(500, {
+            message: parsed_station_view.error.message,
+        });
+    }
+
+    return parsed_station_view.data;
 }) satisfies PageLoad;
