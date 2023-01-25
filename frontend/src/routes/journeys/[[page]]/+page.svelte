21937a9ea670accc40e1ce435acef9e9cfa509c5
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-25 19:18:50 +0200

    Add display component for list of `Journey`'s with support for data fetching

--- frontend/src/routes/journeys/[[page]]/+page.svelte
+++ frontend/src/routes/journeys/[[page]]/+page.svelte
@@ -4,4 +4,44 @@
     export let data: PageData;
 </script>
 
-<h1>Journeys list: page {data.page} </h1>
+<h1>Bicycle journeys: page {data.page} </h1>
+
+{#if data.page > 0}
+    <a href="/journeys/{data.page - 1}">Previous page</a>
+{/if}
+
+{#if (data.journeys.length === data.max_per_page)}
+    <a href="/journeys/{data.page + 1}">Next page</a>
+{/if}
+
+<table>
+    <thead>
+        <tr>
+            <td>Departure date</td>
+            <td>Return date</td>
+            <td>Departure station</td>
+            <td>Return station</td>
+            <td>Distance</td>
+            <td>Duration</td>
+        </tr>
+    </thead>
+
+    {#each data.journeys as journey}
+    <tr>
+        <td>{journey.departure_date}</td>
+        <td>{journey.return_date.toLocaleString()}</td>
+        <td>{journey.departure_station.station_id} | {journey.departure_station.name.finnish} | {journey.departure_station.name.swedish} | {journey.departure_station.name.english}</td>
+        <td>{journey.return_station.station_id} | {journey.return_station.name.finnish} | {journey.return_station.name.swedish} | {journey.return_station.name.english}</td>
+        <td>{journey.distance_kilometers} km</td>
+        <td>{journey.duration_minutes} minutes</td>
+    </tr>
+    {/each}
+</table>
+
+{#if data.page > 0}
+    <a href="/journeys/{data.page - 1}">Previous page</a>
+{/if}
+
+{#if (data.journeys.length === data.max_per_page)}
+    <a href="/journeys/{data.page + 1}">Next page</a>
+{/if}
--- frontend/src/routes/journeys/[[page]]/+page.ts
+++ frontend/src/routes/journeys/[[page]]/+page.ts
@@ -1,10 +1,74 @@
+import { z } from "zod";
+
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (({ params }) => {
+import { PUBLIC_API_ADDRESS } from '$env/static/public'
+
+const MAX_JOURNEYS_PER_PAGE = 100;
+
+const JourneyListViewStation = z.object({
+    station_id: z.number().transform(v => v.toString().padStart(3, '0')),
+    name: z.object({
+        finnish: z.string(),
+        swedish: z.string(),
+        english: z.string(),
+    })
+});
+
+const JourneyListDate = z.string().datetime().transform(v => {
+    let date = new Date(v);
+    
+    // Data has no timezone information available
+    // => format date without taking timezone into account.
+    return date.toLocaleString(undefined, { timeZone: 'UTC'});    
+})
+
+const JourneyListView = z.array(z.object({
+    departure_date: JourneyListDate,
+    return_date: JourneyListDate,
+
+    departure_station: JourneyListViewStation,
+    return_station: JourneyListViewStation,
+
+    distance_kilometers: z.number().transform(v => v.toFixed(3)),
+    duration_minutes: z.number().transform(v => Math.round(v)),
+}));
+
+export const load = (async ({ fetch, params }) => {
+    const page_parse_result = z.coerce.number().safeParse(params.page);
+
+    console.log(page_parse_result);
+
+    const page = (page_parse_result.success) ? page_parse_result.data : 0;
+    
+    const res = await fetch(`${PUBLIC_API_ADDRESS}/journey/list?page=${page}`);
+    const journey_json = await res.json();
+
+    if(!res.ok) {
+        throw error(res.status, {
+            message: journey_json,
+        });
+    }
+
+    let parsed_journey_list_view = JourneyListView.safeParse(journey_json);
+
+    if (!parsed_journey_list_view.success) {
+        throw error(500, {
+            message: parsed_journey_list_view.error.message,
+        });
+    }
+
+    if (parsed_journey_list_view.data.length === 0) {
+        throw error(404, {
+            message: "Page not found",
+        });
+    }
+
     return {
-        page: params.page || "0",
+        page: page, 
+        max_per_page: MAX_JOURNEYS_PER_PAGE, 
+        journeys: parsed_journey_list_view.data
     };
 
-    throw error(404, 'Not found');
 }) satisfies PageLoad;
