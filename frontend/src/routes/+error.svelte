c62d5f79e71ce890be14220ee84d5c0a556e5579
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-27 13:38:06 +0200

    Add basic error page styling

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores';
+  </script>
+  
+
+  <h3>{$page?.error?.message}</h3>
+
+  <style>
+    h3 {
+        width:100%;
+        display: flex;
+        margin-left:16px;
+    }
+  </style>
