a1c91a883db62472bac26aa91d41aa57e60b6805
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-27 13:14:45 +0200

    Add `Pagination` svelte component
    This component can be added to another componenent that requires
    pagination links. It can be also added multiple times to a single
    component.

---
+++ frontend/src/lib/pagination.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+export let sub_url: string;
+export let current_page: number;
+export let data_amount: number;
+export let data_max_per_page: number;
+
+</script>
+
+<div class="page-links">
+    <div>
+    {#if current_page > 0}
+        <a href="/{sub_url}/{current_page - 1}">Previous page</a>
+    {/if}
+    </div>
+
+    <div class="right">
+        {#if data_amount === data_max_per_page}
+            <a href="/{sub_url}/{current_page + 1}">Next page</a>
+        {/if}
+    </div>
+</div>
+
+<style lang="scss">
+    .page-links {
+        width:100%;
+        display:flex;
+        justify-content: space-between;
+        box-sizing: border-box;
+    }
+
+    .page-links a {
+        display: block;
+        padding:16px;
+    }
+
+    @media only screen and (max-width: 880px) {
+        .page-links {
+            font-size:0.6rem;
+        }
+    }
+</style>
