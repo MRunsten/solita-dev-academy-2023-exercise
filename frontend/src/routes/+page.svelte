7486cfb5451cd11e71163a9b2600796fc55a909c
Markus Runsten <MRunsten@users.noreply.github.com>
2023-01-27 13:08:07 +0200

    Update landing page content and style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+    <h2>Welcome!</h2>
+    <h3>Please choose a dataset.</h3>
+</div>
+
+<style lang="scss">
+    @import '../app.scss';
+
+    div {
+        width:100%;
+        padding:16px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h2,
+    h3 {
+        display: flex;
+        margin-top:16px;
+        font-size:1.35rem;
+        text-align: center;
+        color:$color3-text;
+        padding:4px;
+    }
+
+    h3 {
+        font-size:1.25rem;
+        background:none;
+    }
+
+    p {
+        margin-left:16px;
+    }
+</style>
